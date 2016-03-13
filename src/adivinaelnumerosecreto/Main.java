e8eb47b3eb70b78cb762a72e6a537e98952913de
Jorge Antonio Nicolas Morales <15470297@itcampeche.edu.mx>
2016-03-13 17:20:01 -0600

    Primera version completa del proyecto.

--- src/adivinaelnumerosecreto/Main.java
+++ src/adivinaelnumerosecreto/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package adivinaelnumerosecreto;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +16,28 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Modificacion del metodo Main
+        Scanner entrada = new Scanner(System.in);
+        int numero;
+        boolean seAdivinoElNumeoSecreto = false;
+        Juego juego = new Juego(1, 100, 5);
+        while (juego.puedeIntentarDeNuevo() == true
+                && !seAdivinoElNumeroSecreto) {
+            System.out.print("Adivine el numero secreto: ");
+            numero = entrada.nextInt();
+            seAdivinoElNumeroSecreto = juego.esElNumeroSecreto(numero);
+            if (seAdivinoElNumeroSecreto) {
+                System.out.println("Felicidades. Has adivinado el numero secreto.");
+            }
+            else {
+                System.out.println("Fallaste. El numero secreto es "
+                        + juego.getTextoDeAyuda(numero)
+                        + " que el numero proporcionado");
+                System.out.println("Intenta de nuevo.");
+            }
+        }
+        System.out.println("El numero secreto es " + juego.getNumeroSecreto());
+        
     }
     
 }
--- src/model/Juego.java
+++ src/model/Juego.java
@@ -11,4 +11,66 @@ package model;
  */
 public class Juego {
     
+    // Variables de instancia de la clase Juego
+    private NumeroSecreto numeroSecreto;
+    private int limiteInferior;
+    private int limiteSuperior;
+    private int numeroDeIntentos;
+    private int intentosRealizados;
+    
+    // Dos constantes con nombre (constantes simbolicas)
+    private static final int LIM_INF_PREDET = 1;
+    private static final int LIM_SUP_PREDET = 100;
+    
+    // Implemento  del metodo setRangoDelNumeroSecreto()
+    public void setRangoDelNumeroSecreto(int limiteInferior, int limiteSuperior) {
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        numeroSecreto.setRango(this.limiteInferior, this.limiteSuperior);
+    }
+    
+    // Declare el constructor de la clase Juego
+    public Juego(int limiteInferior, int limiteSuperior, int intentos) {
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+        this.numeroDeIntentos = intentos;
+        this.numeroSecreto = new NumeroSecreto(this.limiteInferior, this.limiteSuperior);
+        this.intentosRealizados = 0;
+    }
+    
+    // Metodo setter para la variables de instancia numeroDeIntentos
+    public void setNumeroDeIntentos (int intentos) {
+        this.numeroDeIntentos = intentos;
+    }
+    
+    // El metodo esElNumeroSecreto() permitira adivinar el numero y utilizara los intentos restantes
+    public  boolean esElNumeroSecreto(int numero) {
+        this.intentosRealizados++;
+        return numeroSecreto.esIgual(numero);
+    }
+    
+    // Metodo puedeIntentarDeNuevo()
+    public boolean puedeIntentarDeNuevo() {
+        boolean hayMasOportunidades = this.intentosRestantes() > 0;
+        return hayMasOportunidades;
+    }
+    
+    // Metodo que permite saber el numero de intentos restantes a utilizar en el juego
+    public int intentosRestantes() {
+        int numeroDeIntentosRestantes = this.numeroDeIntentos - this.intentosRealizados;
+        return (numeroDeIntentosRestantes);
+    }
+    
+    // Metodo getTextoDeAyuda()
+    public String getTextoDeAyuda(int numero) {
+        String texto = null;
+        if (this.numeroSecreto.esMayor(numero) == true) {
+            texto = "mayor";
+        }
+        else if (this.numeroSecreto.esMenor(numero) == true) {
+            texto = "menor";
+        }
+        return texto;
+    }
+    
 }
